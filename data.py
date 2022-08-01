be4c632392635952d4ca8407c4dbf0e4a77acfd6
SRajput1234 <110378347+SRajput1234@users.noreply.github.com>
2022-08-01 12:54:46 +0530

    Update data.py

--- data.py
+++ data.py
@@ -8,8 +8,8 @@ class Data:
 
     buttons = [
         generate_single_button,
-        [InlineKeyboardButton("❣️ 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 ❣️", url="https://t.me/DevilsHeavenMF"),
-         InlineKeyboardButton("🥀 𝐃𝐄𝐏𝐋𝐎𝐏𝐄𝐑 🥀", url="https://t.me/anonymous_was_bot"),
+        [InlineKeyboardButton("❣️ 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 ❣️", url="https://t.me/rajput_ki_haveli"),
+         InlineKeyboardButton("🥀 𝐃𝐄𝐏𝐋𝐎𝐏𝐄𝐑 🥀", url="https://t.me/s_rajputt"),
         ],
     ]
 
@@ -19,6 +19,6 @@ Hᴇʏ {},
 Tʜɪs ɪs {},
 𝐀𝐧 𝐨𝐩𝐞𝐧 𝐬𝐨𝐮𝐫𝐜𝐞 𝐬𝐭𝐫𝐢𝐧𝐠 𝐬𝐞𝐬𝐬𝐢𝐨𝐧 𝐠𝐞𝐧𝐞𝐫𝐚𝐭𝐨𝐫 𝐛𝐨𝐭, 𝐰𝐫𝐢𝐭𝐭𝐞𝐧 𝐢𝐧 𝐩𝐲𝐭𝐡𝐨𝐧 𝐰𝐢𝐭𝐡 𝐭𝐡𝐞 𝐡𝐞𝐥𝐩 𝐨𝐟 🕊️⃝‌🦋𝐒 𝐑𝐚𝐣𝐩𝐮𝐭 𝄟⃟💫 ..
 
-Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://github.com/AnonymousR1025/StringGenBot)
-Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦](https://t.me/anonymous_was_bot) !
+𝐒𝐎𝐔𝐑𝐂𝐄 : [𝐑𝐚𝐣𝐩𝐮𝐭 GITHUB ](https://github.com/SRajput1234/StringGenBot)
+𝐌𝐀𝐃𝐄 𝐁𝐘 : [🕊️⃝‌🦋𝐒 𝐑𝐚𝐣𝐩𝐮𝐭 𝄟⃟💫](https://t.me/s_rajputt) !
     """
